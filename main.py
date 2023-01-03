cb4694efed154eba871b107676eff28ea9ad3673
Juan Jose Sepulveda Calderon <artjsc@outlook.com>
2023-01-02 22:28:42 -0700

    unzip functions was created

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 """
 Proccess Notion exports in order to reduce the image size and place the new image in the markdown file.
 """
+from typing import List
 
 import os
 import logging
 from decouple import config
 
+import zipfile
+
 # load environment variables
 DEBUG = config("DEBUG", default=False, cast=bool)
+
+# CONST VARIABLES
+UNZIP_FILES_PATH = "./unzips/"
 
 # set the logging level
 if DEBUG:
@@ -16,7 +22,7 @@ else:
     logging.basicConfig(level=logging.INFO)
 
 # FUNCTIONS ----------------------------------------------------------------
-def get_zip_files():
+def get_zip_files() -> List[str]:
     """Returns a list of zip files"""
     zip_files = []
     for root, dirs, files in os.walk("./"):
@@ -26,11 +32,24 @@ def get_zip_files():
     
     return zip_files
 
+
+def unzip_file(zip_file: str):
+    """Unzip a zip file"""
+    logging.debug(zip_file)
+    # unzip the file
+    with zipfile.ZipFile(zip_file, "r") as zip_ref:
+        zip_ref.extractall(UNZIP_FILES_PATH)
+
+
 # ENTRY ----------------------------------------------------------------
 def main():
     # find zip files
     zip_files = get_zip_files()
     logging.debug(zip_files)
+
+    # Unzip one fiele
+    unzip_file(zip_files[0])
+
 
 if __name__ == "__main__":
     main()
